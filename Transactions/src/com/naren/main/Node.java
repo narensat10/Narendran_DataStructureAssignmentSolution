43d700dd383e43a6b958120b9ff30f743a65dd62
narensat10 <105964088+narensat10@users.noreply.github.com>
2022-07-31 23:41:16 +0530

    Add files via upload

---
+++ Transactions/src/com/naren/main/Node.java
@@ -0,0 +1,13 @@
+package com.naren.main;
+
+
+public class Node {
+	/*Getting the value of the BST*/
+		int nodeData;
+		Node left,right;
+		Node(int nodeData)
+		{
+			this.nodeData=nodeData;
+			left=right=null;
+		}
+	}
